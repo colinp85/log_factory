b34e291d7da6222fd503ffa811612c2758eb5694
Colin <pattisoncolin@gmail.com>
2018-07-03 20:33:37 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='log_factory',
-    version='0.1.0',
+    version='0.1.1',
     packages=['log_factory', ],
     long_description=open('README.md').read(),
     url='https://github.com/colinp85/log_factory',
